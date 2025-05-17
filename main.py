8c9dd1f241f6b4d09e92ee75395e1a6fc35ddc77
austinljacks <austinjacksforex@gmail.com>
2025-05-17 15:06:06 -0700

    trying sorting logic getting errors

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from stats import get_wordcount
+from stats import get_wordcount, count_characters, chars_dict_to_sorted_list
+
+book_path = "./books/frankenstein.txt"
 
 def get_book_text(file_path):
     contents = ""
@@ -8,10 +10,14 @@ def get_book_text(file_path):
 
     return contents
 
-def main():
-    get_wordcount("./books/frankenstein.txt")
-
-main()
+character_count = count_characters(get_book_text(book_path))
 
 
+
+print("============ BOOKBOT ============")
+print("Analyzing book found at " + book_path + "...")
+print("----------- Word Count ----------")
+get_wordcount(book_path)
+print("--------- Character Count -------")
+print(chars_dict_to_sorted_list(count_characters(get_book_text(book_path))))
     
--- stats.py
+++ stats.py
@@ -6,4 +6,27 @@ def get_wordcount(file_path):
         
     list_content = contents.split()
     num_of_words = len(list_content)
-    print(str(num_of_words) + " words found in the document")
+    print("Found " + str(num_of_words) + " total words")
+
+def count_characters(string):
+    lower_string = string.lower()
+    characters = {}
+
+    for character in  lower_string:
+        if character in characters:
+            characters[character] +=1
+        else:
+            characters[character] = 1
+    
+    return characters
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
