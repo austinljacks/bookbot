fc9ad5903f9727551afb982bee977e887117d220
austinljacks <austinjacksforex@gmail.com>
2025-05-17 13:26:36 -0700

    Created python function in main.py to output book to console and find in path

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+def get_book_text(file_path):
+    contents = ""
+
+    with open(file_path) as f:
+        contents = f.read()
+
+    return contents
+
+def main():
+    print(get_book_text("./books/frankenstein.txt"))
+
+main()
+    
